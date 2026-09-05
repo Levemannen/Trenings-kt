cd5bf6ea9032b1c748136fd22f9f77baf354a94b
Levemannen <Levemannen@gmail.com>
2026-09-05 23:01:53 +0200

    Create list-exercises.js

---
+++ tools/list-exercises.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const vm = require("vm");
+
+global.window = {
+  TreningsbuddyWorkoutCategories: require("../workout-categories.js"),
+};
+global.document = { addEventListener: (_event, callback) => callback() };
+
+let source = fs.readFileSync("app.js", "utf8");
+const marker = "      const filterData";
+source = source.replace(
+  marker,
+  '      console.log(JSON.stringify(allNames)); throw new Error("__DONE__");\n' + marker,
+);
+
+try {
+  vm.runInThisContext(source);
+} catch (error) {
+  if (error.message !== "__DONE__") throw error;
+}
